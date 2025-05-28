1791fb679f7427a5e9ef442723aba0f4c03b64eb
biscuit1800 <perekora71@gmail.com>
2025-05-28 18:14:54 +0300

    feat(auth): implemented JWT authentication and password reset via email
    - Added login and token refresh using SimpleJWT
    - Implemented authenticated user profile view
    - Enabled password reset via email:
    
      - Generates reset token and UID
      - Sends reset link using MailHog
      - Confirm and set new password via a dedicated view
    - Configured routes in users.urls and included them in backend.urls
    
    Tested with Postman and MailHog — everything works as expected ✅

---
+++ backend/users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import ProfileView, PasswordResetView, PasswordResetConfirmView
+
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+
+    #password renew with email
+    path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+]
---
+++ backend/users/views.py
@@ -0,0 +1,73 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer
+
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from rest_framework import status
+
+User = get_user_model()
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+
+class PasswordResetView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        email = request.data.get('email')
+        if not email:
+            return Response({"error": "Email is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({"error": "User with this email does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        reset_link = f"http://127.0.0.1:8000/api/auth/password/reset/confirm/?uid={uid}&token={token}"
+
+
+        send_mail(
+            subject="Password Reset",
+            message=f"Use the link to reset your password: {reset_link}",
+            from_email="no-reply@example.com",
+            recipient_list=[email],
+            fail_silently=False,
+        )
+        return Response({"message": "Password reset link sent"}, status=status.HTTP_200_OK)
+
+
+class PasswordResetConfirmView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        uidb64 = request.query_params.get('uid')
+        token = request.query_params.get('token')
+        new_password = request.data.get('new_password')
+
+        if not uidb64 or not token or not new_password:
+            return Response({"error": "Missing parameters"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            return Response({"error": "Invalid uid"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not default_token_generator.check_token(user, token):
+            return Response({"error": "Invalid or expired token"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+        return Response({"message": "Password has been reset"}, status=status.HTTP_200_OK)
