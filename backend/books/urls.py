b6c0832a74a1d4351a8de18c638174a9be2461af
biscuit1800 <perekora71@gmail.com>
2025-05-29 11:01:18 +0300

    books app work?

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from .views import ScrapeBooksView
 
 router = DefaultRouter()
 router.register(r'', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('scrape/', ScrapeBooksView.as_view(), name='scrape-books'),
 ]
--- backend/books/views.py
+++ backend/books/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from books.utils import scrape_all_books
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -14,3 +18,9 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_classes = [IsAdminOrReadOnly]
 
+class ScrapeBooksView(APIView):
+    def get(self, request):
+        pages = int(request.query_params.get('pages', 1))
+        pages = max(1, min(pages, 10))  # обмеження 1–10
+        books = scrape_all_books(pages)
+        return Response({"count": len(books), "books": books}, status=status.HTTP_200_OK)
