15c8355b41d1d7a30ed3b74d8b70ff8fe323408a
bubluk <v1taka0>
2025-05-29 20:44:19 +0000

    testing cicd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -165,9 +165,8 @@ REST_FRAMEWORK = {
 }
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+    "default": {
+        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
     }
 }
 
