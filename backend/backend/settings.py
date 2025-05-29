9ebe3f68a5f3a52eadfdd43326f6c8cb7946fd98
bubluk <v1taka0>
2025-05-29 18:59:31 +0000

    test pipelines

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
