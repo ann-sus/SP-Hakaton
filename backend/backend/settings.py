99fb9be64a337efb9cb74a7cd51862de24ea6de2
bubluk <v1taka0>
2025-05-29 19:55:45 +0000

    added static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,12 +149,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend' / 'public',
-]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # або /app/public/static в Docker
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
