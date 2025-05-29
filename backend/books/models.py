a93feb60e4fb5eb96dbc0c298d6e452b8a887162
Ann <annnchernenk000@gmail.com>
2025-05-29 13:31:04 +0300

    добавлено вивантаження книг

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     availability = models.CharField(max_length=100)
-    genre = models.CharField(max_length=100, blank=True)
-    publication_year = models.IntegerField(null=True, blank=True)
+    rating = models.CharField(max_length=20)
+    genre = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
--- backend/books/views.py
+++ backend/books/views.py
@@ -1,47 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .utils import scrape_all_books  #ІМПОРТ
-# books/views.py
+
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 from .permissions import IsAdminOrReadOnly
 
+from .utils import scrape_all_books
+
+
 class ScrapeBooksView(APIView):
     def get(self, request):
         page_count = int(request.query_params.get('pages', 1))
+        save = request.query_params.get('save', 'false').lower() == 'true'
+
         if page_count < 1 or page_count > 10:
             return Response({"error": "Pages must be between 1 and 10"}, status=400)
 
-        books = scrape_all_books(page_count)  #ВИКЛИК
+        books = scrape_all_books(page_count, save_to_db=save)
         return Response({"count": len(books), "books": books})
-
-    def scrape_books(self, max_pages):
-        BASE_URL = "https://books.toscrape.com/catalogue/page-{}.html"
-        all_books = []
-
-        for page in range(1, max_pages + 1):
-            url = BASE_URL.format(page)
-            response = requests.get(url)
-            if response.status_code != 200:
-                continue
-
-            soup = BeautifulSoup(response.text, 'html.parser')
-            for book in soup.select("article.product_pod"):
-                title = book.h3.a['title']
-                price = book.select_one("p.price_color").text.strip()
-                availability = book.select_one("p.instock.availability").text.strip()
-
-                all_books.append({
-                    "title": title,
-                    "price": price,
-                    "availability": availability
-                })
-
-        return all_books
-
-
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
