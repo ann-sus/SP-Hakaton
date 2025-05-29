aa4f3ea27daae8c2ab43978ed47cce26524a5937
bubluk <v1taka0>
2025-05-29 19:01:28 +0000

    test pipelines

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,6 +152,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
