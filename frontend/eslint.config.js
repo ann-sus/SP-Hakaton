1f6729811418bf45a2e24d6a4659f97b5c7ab5af
bubluk <v1taka0>
2025-05-29 18:20:20 +0000

    test pipelines

---
+++ frontend/eslint.config.js
@@ -0,0 +1,10 @@
+// frontend/eslint.config.js
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+];
