ea77e4518d940eadb6b54c2e33353ac02b3a51c9
Wuradzi <wuradzi@gmail.com>
2025-05-30 13:40:58 +0300

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { withTokenRefresh } from "./utils/withTokenRefresh";
 
 import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
@@ -22,29 +21,23 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("root element not found");
 const root = createRoot(rootElement);
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div style={{ display: "inline-block", width: "100%", background: "#2d2c2a" }} data-ignore="used only for top most containter width">
-        <LibraryHub />
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/books" element={<MyBooksPage />} />
-          <Route path="/books/:id" element={<BookDescription />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/reset-password/confirm" element={<PasswordResetConfirmPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
-}
-
-const AppWithTokenRefresh = withTokenRefresh(App);
-
-root.render(<AppWithTokenRefresh />);
+root.render(
+  <BrowserRouter>
+    <div style={{ display: "inline-block", width: "100%", background: "#2d2c2a" }} data-ignore="used only for top most containter width">
+      <LibraryHub />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/books" element={<MyBooksPage />} />
+        <Route path="/books/:id" element={<BookDescription />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/reset-password/confirm" element={<PasswordResetConfirmPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
